51f89a9ced276de237012019da8bfbb8539ae4a1
Michael Cuffaro <consulting@michaelcuffaro.com>
2021-11-26 14:50:31 +0100

    check datatype conditions in validate_cell(); run code through black

--- src/script/validate.py
+++ src/script/validate.py
@@ -2,6 +2,7 @@
 
 import re
 
+
 def validate_rows(config, table_name, rows):
     """Given a validation config, a table name,
     and a list of row dicts (from column names to value strings),
@@ -11,11 +12,12 @@ def validate_rows(config, table_name, rows):
         result_row = {}
         for column, value in row.items():
             result_row[column] = {
-                'value': value,
-                'valid': True,
+                "value": value,
+                "valid": True,
             }
         result_rows.append(validate_row(config, table_name, result_row))
     return result_rows
+
 
 def validate_row(config, table_name, row):
     """Given a validation config, a table name,
@@ -25,18 +27,50 @@ def validate_row(config, table_name, row):
         row[column_name] = validate_cell(config, table_name, column_name, cell)
     return row
 
+
 def validate_cell(config, table_name, column_name, cell):
     """Given a validation config, a table name, a column name, and a cell dict,
     return an updated cell dict."""
-    # TODO: Hierarchical validation
-    column = config['table'][table_name]['column'][column_name]
-    nt_name = column['nulltype']
+    # If the value of the cell is one of the allowable null-type values for this column, then mark
+    # it as such:
+    column = config["table"][table_name]["column"][column_name]
+    nt_name = column["nulltype"]
     if nt_name:
-        nulltype = config['datatype'][nt_name]
-        result = validate_condition(nulltype['condition'], cell['value'])
+        nulltype = config["datatype"][nt_name]
+        result = validate_condition(nulltype["condition"], cell["value"])
         if result:
-            cell['nulltype'] = nt_name
+            cell["nulltype"] = nt_name
+
+    # Validate that the value of the cell conforms to the datatypes associated with the column:
+    def get_datatypes_to_check(dt_name):
+        datatypes = []
+        if dt_name is not None:
+            datatype = config["datatype"][dt_name]
+            if datatype["condition"] is not None:
+                datatypes.append(datatype)
+            datatypes += get_datatypes_to_check(datatype["parent"])
+        return datatypes
+
+    cell["messages"] = []
+    if not cell.get("nulltype"):
+        dt_name = column["datatype"]
+        datatypes_to_check = get_datatypes_to_check(dt_name)
+        # We use while and pop() instead of a for loop so as to check conditions in LIFO order:
+        while datatypes_to_check:
+            datatype = datatypes_to_check.pop()
+            if datatype["condition"].startswith("exclude") == validate_condition(
+                datatype["condition"], cell["value"]
+            ):
+                cell["messages"].append(
+                    {
+                        "rule": "datatype:{}".format(datatype["datatype"]),
+                        "level": "error",
+                        "message": "{} should be {}".format(column_name, datatype["description"]),
+                    }
+                )
+                cell["valid"] = False
     return cell
+
 
 def validate_condition(condition, value):
     """Given a condition string and a value string,
@@ -44,18 +78,17 @@ def validate_condition(condition, value):
     # TODO: Implement real conditions.
     if condition == "equals('')":
         return value == ""
-    elif condition == "exclude(/\n/)":
-        return '\n' in value
-    elif condition == "exclude(/\s/)":
-        return bool(re.search('\s', value))
-    elif condition == "exclude(/^\s+|\s+$/)":
-        return bool(re.search('^\s+|\s+$', value))
-    elif condition == "in('table', 'column', 'datatype')":
-        return value in ('table', 'column', 'datatype')
-    elif condition == "match(/\w+/)":
-        return bool(re.matches('\w+', value))
-    elif condition == "search(/:/)":
-        return ':' in value
+    elif condition == r"exclude(/\n/)":
+        return "\n" in value
+    elif condition == r"exclude(/\s/)":
+        return bool(re.search(r"\s", value))
+    elif condition == r"exclude(/^\s+|\s+$/)":
+        return bool(re.search(r"^\s+|\s+$", value))
+    elif condition == r"in('table', 'column', 'datatype')":
+        return value in ("table", "column", "datatype")
+    elif condition == r"match(/\w+/)":
+        return bool(re.fullmatch(r"\w+", value))
+    elif condition == r"search(/:/)":
+        return ":" in value
     else:
         raise Exception(f"Unhandled condition: {condition}")
-
