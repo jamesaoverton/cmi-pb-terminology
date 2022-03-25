bc0cff193e34f10da288ad9bacb0bf147efc263c
Rebecca Jackson <rbca.jackson@gmail.com>
2022-03-24 21:18:33 -0700

    Fix setup

---
+++ src/run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.9
+import os
+
+from cmi_pb_server.run import run
+
+if __name__ == "__main__":
+    os.chdir("..")
+    run(
+        "build/cmi-pb.db",
+        "src/table.tsv",
+        cgi_path="/CMI-PB/branches/next/views/src/run.py",
+        log_file="app.log"
+    )
--- src/setup.py
+++ src/setup.py
@@ -2,17 +2,19 @@ from setuptools import setup, find_packages
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
-
-install_requires = ["ontodev-gizmos"]
+install_requires = [
+    "Flask==2.0.2",
+    "lark==1.0.0",
+    "SQLAlchemy==1.4.29",
+    "xlsx2csv",
+    # "ontodev-gizmos @ git+https://github.com/ontodev/gizmos.git",
+    # "ontodev-sprocket @ git+https://github.com/ontodev/sprocket.git"
+]
 
 setup(
     name="cmi-pb-terminology",
     version="0.0.1",
     description="Terminology tools for the Computational Modelling of Immunology Pertussis Boost project.",
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jamesaoverton/cmi-pb-terminology",
     author="James A. Overton",
@@ -27,4 +29,5 @@ setup(
     install_requires=install_requires,
     packages=find_packages(exclude="test"),
     python_requires=">=3.6, <4",
+    package_data={"cmi_pb_server": ["templates/*.html"]}
 )
